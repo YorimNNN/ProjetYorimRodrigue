bb4a141776fd3ebaf30b6e9c71f1d5ae25b534e9
Rod76m <rodrigue.mrt@gmail.com>
2022-02-20 18:47:21 +0100

    Accueil
    accueil

---
+++ ProjetYorimRodrigue.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+      <title> A changer</title>
+      <link href="test.css" rel="stylesheet" /> 
+      <h1>Actualités sportive</h1>
+  </head>
+ 
+    <body>
+      <div class="article">
+        <div>
+          <h2>Article </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum."
+          </p>
+        
+       </div>
+        <div>
+          <h2>Article </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+        <div>
+          <h2>Article </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+          ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+            qui officia deserunt mollit anim id est laborum."
+          </p>
+        </div>
+
+      </div>
+    </body>
+    
+</html>
